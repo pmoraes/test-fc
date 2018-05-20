c4662e243c8036ee8254b93c1e8e18cb1a72aa0e
Peter Moraes <peter.veiga.moraes@gmail.com>
2018-05-20 04:11:31 -0300

    updating database dump

--- docker/dump/test.sql
+++ docker/dump/test.sql
@@ -1,18 +1,80 @@
+-- MySQL dump 10.13  Distrib 5.7.21, for Linux (x86_64)
+--
+-- Host: localhost    Database: test
+-- ------------------------------------------------------
+-- Server version	5.7.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `families`
+--
+
 DROP TABLE IF EXISTS `families`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `families` (
-  `id` integer(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `quantity_members` integer(11) NOT NULL,
+  `quantity_members` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `families`
+--
+
+LOCK TABLES `families` WRITE;
+/*!40000 ALTER TABLE `families` DISABLE KEYS */;
+INSERT INTO `families` VALUES (1,'Start',8),(2,'Lannister',12),(3,'Tyrell',4),(4,'Baratheon',5);
+/*!40000 ALTER TABLE `families` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `wars`
+--
 
 DROP TABLE IF EXISTS `wars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wars` (
-  `id` integer(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `date_start` datetime NOT NULL,
   `date_end` datetime NOT NULL,
-  `family_id_challenging` integer(11) NOT NULL,
-  `family_id_challenged` integer(11) NOT NULL,
-  `family_id_winner` integer(11) NOT NULL,
+  `family_id_challenging` int(11) NOT NULL,
+  `family_id_challenged` int(11) NOT NULL,
+  `family_id_winner` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wars`
+--
+
+LOCK TABLES `wars` WRITE;
+/*!40000 ALTER TABLE `wars` DISABLE KEYS */;
+INSERT INTO `wars` VALUES (4,'2018-05-18 00:00:00','2018-05-20 00:00:00',1,2,1),(5,'2018-05-17 00:00:00','2018-05-23 00:00:00',2,3,2),(6,'2018-05-18 00:00:00','2018-05-23 00:00:00',4,3,4),(7,'2018-05-23 00:00:00','2018-05-26 00:00:00',2,4,2),(8,'2018-05-15 00:00:00','2018-05-16 00:00:00',2,1,2);
+/*!40000 ALTER TABLE `wars` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-20  7:11:07
